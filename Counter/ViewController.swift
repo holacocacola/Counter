6a9be9f0e1cb7d470b2f24f206ee5fa4fe6335d7
holacocacola <40626191+holacocacola@users.noreply.github.com>
2025-10-19 12:01:20 +0300

    Реализовано задание спринта 3 (#1)
    * Реализовано задание спринта 3
    
    * Исправления после ревю
    
    ---------
    
    Co-authored-by: Practicum <practicum@yandex.ru>

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  Counter
@@ -7,13 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// MARK: - ViewController
 
+final class ViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var textHistory: UITextView!
+    
+    // MARK: - Properties
+    
+    private var counter: Int = 0 {
+        didSet { updateLabel() }
+    }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLabel()
     }
 
+    // MARK: - Private Methods
+    
+    private func updateLabel() {
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
+    
+    private func addHistory(_ text: String) {
+        // Преобразуем текущую дату в текст с помощью заранее созданного форматтера.
+        let dateString = dateFormatter.string(from: Date())
+        
+        // Добавляем запись в историю.
+        textHistory.text += "\n[\(dateString)]: \(text)"
+        
+        // Автоматически прокручиваем текст к последней строке.
+        let range = NSMakeRange(textHistory.text.count, 0)
+        textHistory.scrollRangeToVisible(range)
+    }
 
+    // MARK: - Actions
+    
+    @IBAction private func minusButton(_ sender: Any) {
+        // Значение счётчика не должно падать ниже нуля.
+        if counter > 0 {
+            counter -= 1
+            addHistory("значение изменено на -1")
+        } else {
+            addHistory("попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    @IBAction private func plusButton(_ sender: Any) {
+        counter += 1
+        addHistory("значение изменено на +1")
+    }
+    
+    @IBAction private func resetButton(_ sender: Any) {
+        counter = 0
+        addHistory("значение сброшено")
+    }
 }
-
